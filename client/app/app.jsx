ab29a7d2963644c4cd1195e8bd727b155ecab74c
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-05-15 22:16:40 +0300

    Change text on submit button from form component state

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -17,7 +17,7 @@ var App=React.createClass({
 					text: '',
 					completed: false
 				},
-				action: ''
+				action: 'CREATE'
 			} 
 		};
 	},
--- client/app/noteform.jsx
+++ client/app/noteform.jsx
@@ -2,30 +2,43 @@ import React from 'react';
 import utils from './utils'
 var NoteForm = React.createClass({
 	getInitialState: function () {
-		return this.props.noteEdit.data;
+		return this.props.noteEdit;
 	},
 
 	noteChange: function (e) {
 		this.setState({
-			id : this.state.id || '',
-			name : e.target.name == "name" ? e.target.value : this.state.name,
-			date :e.target.name == "date" ? e.target.value : this.state.date,
-			text :e.target.name == "text" ? e.target.value : this.state.text,
-			completed :e.target.name == "completed" ? e.target.checked : this.state.completed
-		});
+			noteEdit:{
+				data:{
+					id : this.state.data.id || '',
+					name : e.target.name == 'name' ? e.target.value : this.state.data.name,
+					date :e.target.name == 'date' ? e.target.value : this.state.data.date,
+					text :e.target.name == 'text' ? e.target.value : this.state.data.text,
+					completed :e.target.name == 'completed' ? e.target.checked : this.state.data.completed
+				}
+			}
+		}
+		);
 	},
 
 	componentWillReceiveProps: function(nextProps) {
-		console.log(nextProps.noteEdit)
-		if(nextProps.noteEdit.action=="DELETE"){
+		if(nextProps.noteEdit.action=='DELETE'){
 			this.clearForm()
 			return
 		}
 		let {date,id,text,completed,name} = nextProps.noteEdit.data;
+		console.log('CWLRP: ',nextProps.noteEdit);
 		let {YYYY,MM,DD}=utils.convertDate(date);
-  	this.setState({date:`${YYYY}-${MM}-${DD}`,id,text,completed,name});
-
-	},
+  	this.setState({
+				data:{
+					date:`${YYYY}-${MM}-${DD}`,
+					id,
+					text,
+					completed,
+					name
+				},
+				action: 'EDIT'
+		});
+  },
 
 	onSubmit: function (e) {
 		e.preventDefault();
@@ -44,36 +57,46 @@ var NoteForm = React.createClass({
 
 	clearForm: function () {
 		this.setState({
-			id: '',
-			name: '',
-			date: '',
-			text: '',
-			completed: false	
+			data:{
+				id: '',
+				name: '',
+				date: '',
+				text: '',
+				completed: false	
+			},
+			action: 'CREATE'
 		})
+	},
+	stateButtonName:function () {
+		console.log(this.state)
+		if(this.state.action==='CREATE')
+			return 'Create'
+		if(this.state.action==='EDIT')
+			return 'Update'
 	},
 	render: function(){
 		return(
 			<form onSubmit={this.onSubmit}>
-				<input type="hidden" name="id" value={this.state.id}/>
+				<input type="hidden" name="id" value={this.state.data.id}/>
 				<div className="form-group">
 					<label>Имя</label>
-				  	<input type="text" required value={this.state.name} onChange={this.noteChange} name="name" className="form-control" placeholder="Имя"/>
+				  	<input type="text" required value={this.state.data.name} onChange={this.noteChange} name="name" className="form-control" placeholder="Имя"/>
 				  	
 				</div>
 				<div className="form-group">
 					<label>Дата</label>
-				  	<input type="date" required value={this.state.date} onChange={this.noteChange} name="date" className="form-control" placeholder="Дата"/>
+				  	<input type="date" required value={this.state.data.date} onChange={this.noteChange} name="date" className="form-control" placeholder="Дата"/>
 				</div>
 				<div className="form-group">
 					<label>Текст</label>
-						<textarea cols="30" rows="10" required value={this.state.text} onChange={this.noteChange} name="text" className="form-control" placeholder="Описание" />
+						<textarea cols="30" rows="10" required value={this.state.data.text} onChange={this.noteChange} name="text" className="form-control" placeholder="Описание" />
 				</div>
 				<div className="checkbox">
 			    <label>
-			      <input type="checkbox" checked={this.state.completed} onChange={this.noteChange} name="completed"/> Completed
+			      <input type="checkbox" checked={this.state.data.completed} onChange={this.noteChange} name="completed"/> Completed
 			    </label>
 			  </div>
-					<button type="submit" className="btn btn-primary">Create</button>&nbsp;
+					<button type="submit" className="btn btn-primary">{this.stateButtonName()}</button>&nbsp;
 					<button type="reset" onClick={this.clearForm} className="btn btn-warning">Reset form</button>
 			</form>
 		)
