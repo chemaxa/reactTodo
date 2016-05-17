1da21482a202f9fc76f51f0ca233bc8369f022d9
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-05-17 12:42:46 +0300

    Create dashboard & Menu components

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import About from './about'
+import Home from './home'
+import Dashboard from './dashboard'
+import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(
+ <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="dashboard" component={Dashboard} />
+    </Route>
+  </Router>
+  ,document.getElementById('App'));
