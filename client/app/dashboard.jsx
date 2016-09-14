c84222746e1e1b729d79d4474d23481b62cee9d5
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-09-14 13:36:17 +0300

    Code format

--- client/app/dashboard.jsx
+++ client/app/dashboard.jsx
@@ -4,13 +4,13 @@ import NotesList from './noteslist';
 import Controls from './controls';
 import noteModel from './notemodel';
 
-class Dashboard extends Component{
-		constructor(props){
+class Dashboard extends Component {
+	constructor(props) {
 		super(props);
-		console.log('Dashboard props: ',props);
-		
-		this.state={
-			notes:[],
+		console.log('Dashboard props: ', props);
+
+		this.state = {
+			notes: [],
 			noteEdit: {
 				data: {
 					_id: '',
@@ -21,7 +21,7 @@ class Dashboard extends Component{
 				},
 				action: 'CREATE'
 			},
-			sorting:{
+			sorting: {
 				param: '',
 				asc: false
 			},
@@ -31,59 +31,25 @@ class Dashboard extends Component{
 		};
 
 		this.onSort = this.onSort.bind(this);
-		this.setCompleted=this.setCompleted.bind(this);
-		this.onEdit=this.onEdit.bind(this);
-		this.onUpdate=this.onUpdate.bind(this);
-		this.onDelete=this.onDelete.bind(this);
-		this.onAdd=this.onAdd.bind(this);
-		this.render=this.render.bind(this);
-		this.clearForm=this.clearForm.bind(this);
-		this.onSearch=this.onSearch.bind(this);
-		this.closeAlert=this.closeAlert.bind(this);
-		this.clearCompleted=this.clearCompleted.bind(this);
+		this.setCompleted = this.setCompleted.bind(this);
+		this.onEdit = this.onEdit.bind(this);
+		this.onUpdate = this.onUpdate.bind(this);
+		this.onDelete = this.onDelete.bind(this);
+		this.onAdd = this.onAdd.bind(this);
+		this.render = this.render.bind(this);
+		this.clearForm = this.clearForm.bind(this);
+		this.onSearch = this.onSearch.bind(this);
+		this.closeAlert = this.closeAlert.bind(this);
+		this.clearCompleted = this.clearCompleted.bind(this);
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		noteModel
 			.getList()
-			.then((data)=>{
+			.then((data) => {
 				let notes = JSON.parse(data),
-						noteEdit = {
-							data: {
-								_id: '',
-								name: '',
-								date: '',
-								text: '',
-								completed: false
-							},
-							action: 'CREATE'
-						};
-			
-					if(this.props.params.todoId){
-						noteEdit.data=notes.filter((item)=>{
-							return item._id===this.props.params.todoId
-					})[0];
-				}
-				
-				this.setState({
-					noteEdit,
-					notes,
-					isLoading: false
-				})
-				this.initialData=notes.slice();
-				
-			})
-			.catch((error)=>{
-				console.error(error);
-			})
-		
-			//console.log(this.props.params.todoId);
-	}
-	componentWillReceiveProps(nextProps) {
-		if(!nextProps.params.todoId){
-			this.setState({
-				noteEdit: {
-					data: {
+					noteEdit = {
+						data: {
 							_id: '',
 							name: '',
 							date: '',
@@ -91,18 +57,52 @@ class Dashboard extends Component{
 							completed: false
 						},
 						action: 'CREATE'
-					}
+					};
+
+				if (this.props.params.todoId) {
+					noteEdit.data = notes.filter((item) => {
+						return item._id === this.props.params.todoId
+					})[0];
+				}
+
+				this.setState({
+					noteEdit,
+					notes,
+					isLoading: false
+				})
+				this.initialData = notes.slice();
+
+			})
+			.catch((error) => {
+				console.error(error);
+			})
+
+		//console.log(this.props.params.todoId);
+	}
+	componentWillReceiveProps(nextProps) {
+		if (!nextProps.params.todoId) {
+			this.setState({
+				noteEdit: {
+					data: {
+						_id: '',
+						name: '',
+						date: '',
+						text: '',
+						completed: false
+					},
+					action: 'CREATE'
+				}
 			});
 		}
 	}
 
-	onSort (param){
+	onSort(param) {
 		let notes = this.state.notes.slice();
 		let asc = this.state.sorting.asc;
 		let ordr = asc ? 1 : -1;
-		notes.sort((item1, item2)=>{
-			if(item1[param]===item2[param]) return 0;
-			return item1[param]>item2[param] ? ordr*1 : ordr*-1;
+		notes.sort((item1, item2) => {
+			if (item1[param] === item2[param]) return 0;
+			return item1[param] > item2[param] ? ordr * 1 : ordr * -1;
 		});
 		this.setState({
 			notes: notes,
@@ -113,110 +113,110 @@ class Dashboard extends Component{
 		})
 	}
 
-	onSearch(term){
+	onSearch(term) {
 		let notes = this.initialData.slice();
-		let filter = notes.filter(item=> ~(item.name.toLowerCase().indexOf(term)))
+		let filter = notes.filter(item => ~(item.name.toLowerCase().indexOf(term)))
 		this.setState({
 			term: term,
 			notes: filter
 		})
 	}
 
-	onEdit(note){
+	onEdit(note) {
 		this.setState({
 			noteEdit: {
 				data: note,
 				action: 'EDIT'
 			}
 		});
- 		this.context.router.push(`/dashboard/${note._id}`);
+		this.context.router.push(`/dashboard/${note._id}`);
 	}
-	
-	clearCompleted(){
-		let completed = this.state.notes.filter(item=>item.completed);
+
+	clearCompleted() {
+		let completed = this.state.notes.filter(item => item.completed);
 		noteModel
 			.clearCompleted(completed)
-			.then((data)=>{
+			.then((data) => {
 				let notes = JSON.parse(data);
-				this.setState({notes: notes});
-				this.initialData=notes;
+				this.setState({ notes: notes });
+				this.initialData = notes;
 			})
-			.catch((error)=>{
+			.catch((error) => {
 				console.error(error);
 			});
 	}
 
-	setCompleted (note) {
-		note.completed=!note.completed;
+	setCompleted(note) {
+		note.completed = !note.completed;
 		noteModel
 			.updateNote(note)
-			.then((data)=>{
+			.then((data) => {
 				let notes = JSON.parse(data);
-				this.setState({notes})
-				this.initialData=notes.slice();
+				this.setState({ notes })
+				this.initialData = notes.slice();
 			})
-			.catch((error)=>{
+			.catch((error) => {
 				console.error(error);
 			});
 	}
 
-	onUpdate(note){
+	onUpdate(note) {
 		noteModel
 			.updateNote(note)
-			.then((data)=>{
+			.then((data) => {
 				let notes = JSON.parse(data);
 				this.setState({
 					notes,
-					noteEdit:{
-			  	data: note,
-			  	action: 'UPDATE'
-			  }
-			 });
-			 this.initialData=notes;
+					noteEdit: {
+						data: note,
+						action: 'UPDATE'
+					}
+				});
+				this.initialData = notes;
 			})
-			.catch((error)=>{
+			.catch((error) => {
 				console.error(error);
 			});
 	}
 
-	onDelete(note){
-  noteModel
+	onDelete(note) {
+		noteModel
 			.deleteNote(note._id)
-			.then((data)=>{
+			.then((data) => {
 				let notes = JSON.parse(data);
-				this.setState({ 
-			 	notes: notes
-			 });
-			 this.initialData=notes;
+				this.setState({
+					notes: notes
+				});
+				this.initialData = notes;
 			})
-			.catch((error)=>{
+			.catch((error) => {
 				console.error(error);
 			});
 	}
-	
-	onAdd (note){
+
+	onAdd(note) {
 		noteModel
-		 .createNote(note)
-			.then((data)=>{
+			.createNote(note)
+			.then((data) => {
 				let notes = JSON.parse(data);
-				this.setState({notes: notes});
-				this.initialData=notes;
+				this.setState({ notes: notes });
+				this.initialData = notes;
 			})
-			.catch((error)=>{
+			.catch((error) => {
 				console.error(error);
 			});
 		this.onSearch('');
 	}
 
-	clearForm(){
+	clearForm() {
 		this.setState({
-			noteEdit:{
-				data:{
+			noteEdit: {
+				data: {
 					_id: '',
 					name: '',
 					date: '',
 					text: '',
-					completed: false	
+					completed: false
 				},
 				action: 'CREATE'
 			}
@@ -224,25 +224,25 @@ class Dashboard extends Component{
 		this.context.router.push('/dashboard')
 	}
 
-	closeAlert(){
+	closeAlert() {
 		this.setState({
-			messages:{}
+			messages: {}
 		})
 	}
 
-	render(){
-		return(
+	render() {
+		return (
 			<div className="row">
 				<div className="col-md-5">
 					<Controls
-							term={this.state.term}
-							onSearch={this.onSearch}
-							notes={this.state.notes}
-							sorting={this.onSort}
-							sortState={this.state.sorting}
-							clearCompleted={this.clearCompleted}
+						term={this.state.term}
+						onSearch={this.onSearch}
+						notes={this.state.notes}
+						sorting={this.onSort}
+						sortState={this.state.sorting}
+						clearCompleted={this.clearCompleted}
 						/>
-					<NoteForm 
+					<NoteForm
 						onAdd={this.onAdd}
 						onUpdate={this.onUpdate}
 						onEdit={this.onEdit}
@@ -251,17 +251,17 @@ class Dashboard extends Component{
 						notes={this.state.notes}
 						messages={this.state.messages}
 						closeAlert={this.closeAlert}
-					/>
+						/>
 				</div>
 				<div className="col-md-7">
 					<NotesList
 						isLoading={this.state.isLoading}
-						notes={this.state.notes} 
-						onEdit={this.onEdit} 
-						setCompleted={this.setCompleted} 
-						onDelete={this.onDelete} 
+						notes={this.state.notes}
+						onEdit={this.onEdit}
+						setCompleted={this.setCompleted}
+						onDelete={this.onDelete}
 						noteEdit={this.state.noteEdit}
-					/>
+						/>
 				</div>
 			</div>
 		)
@@ -270,6 +270,6 @@ class Dashboard extends Component{
 
 
 Dashboard.contextTypes = {
-  router: PropTypes.object.isRequired
+	router: PropTypes.object.isRequired
 }
 export default Dashboard
