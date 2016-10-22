6d6640dd0d3adce132490e7250432a8e9ea6c94f
Maxim Shkarbalyuk <chemaxa@gmail.com>
2016-10-22 20:10:39 +0300

    Add login button

--- client/app/menu.jsx
+++ client/app/menu.jsx
@@ -5,13 +5,16 @@ class Menu extends Component{
 		let {pathname} = this.props.location;
 		return(
 			<nav className="navbar navbar-default">
-			 	<ul className="nav navbar-nav">
-			 		<li className={pathname == "/" ? "active" : ""}><Link to="/">Home</Link></li>
-          <li className={pathname == "/dashboard" ? "active" : ""}><Link to="/dashboard">Dashboard</Link></li>
-          <li className={pathname == "/about" ? "active" : ""}><Link to="/about">About</Link></li>
-        </ul>
+				<div className="container-fluid">
+					<ul className="nav navbar-nav">
+						<li className={pathname == "/" ? "active" : ""}><Link to="/">Home</Link></li>
+						<li className={pathname == "/dashboard" ? "active" : ""}><Link to="/dashboard">Dashboard</Link></li>
+						<li className={pathname == "/about" ? "active" : ""}><Link to="/about">About</Link></li>
+					</ul>
+					<button type="button"  data-toggle="modal" data-target="#loginModal" className="navbar-right btn btn-primary navbar-btn">Sign in</button>
+				</div>
 	    </nav>
-			)
+		)
 	}
 }
 
